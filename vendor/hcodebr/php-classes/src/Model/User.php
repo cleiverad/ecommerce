efb8978e93da06f7124698f7e7561f1f90cc723c
Cleiver Dutra <cleiverad@gmail.com>
2019-07-04 15:17:47 -0300

    Crud do usuario

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -62,7 +62,69 @@ Class User extends Model{
 
 	}
 
+	public static function listAll(){
 
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	public function save(){
+
+		$Sql = new Sql();
+
+		$results = $Sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			"desperson"=>$this->getdesperson(),
+			"deslogin"=>$this->getdeslogin(),
+			"despassword"=>$this->getdespassword(),
+			"desemail"=>$this->getdesemail(),
+			"nrphone"=>$this->getnrphone(),
+			"inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+
+	}
+
+	public function get($iduser){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+		
+		$this->setData($results[0]);
+	}
+
+	public function update(){
+
+		$Sql = new Sql();
+
+		$results = $Sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			"iduser"=>$this->getiduser(),
+			"desperson"=>$this->getdesperson(),
+			"deslogin"=>$this->getdeslogin(),
+			"despassword"=>$this->getdespassword(),
+			"desemail"=>$this->getdesemail(),
+			"nrphone"=>$this->getnrphone(),
+			"inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+
+	}
+
+	public function delete(){
+
+		$Sql = new Sql();
+
+		$Sql->select("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+
+	}
 }
 
  ?>
