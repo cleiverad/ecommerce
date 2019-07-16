6b4d104c829432d6f4c1642dd565202110e920e0
Cleiver Dutra <cleiverad@gmail.com>
2019-07-16 10:18:57 -0300

    Carrinho de Compras com produtos

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -107,6 +107,57 @@ Class Cart extends Model{
 
 	}
 
+	public function addProduct(Product $product){
+
+		$Sql = new Sql();
+
+		$Sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES (:idcart, :idproduct)", [
+			":idcart"=>$this->getidcart(),
+			":idproduct"=>$product->getidproduct()
+		]);
+
+	}
+
+	public function removeProduct(Product $product, $all = false){
+
+		$Sql = new Sql();
+
+		if($all) {
+
+			$Sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved is NULL", [
+			":idcart"=>$this->getidcart(),
+			":idproduct"=>$product->getidproduct()
+		]);
+
+		} else {
+
+			$Sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+			":idcart"=>$this->getidcart(),
+			":idproduct"=>$product->getidproduct()
+		]);
+
+		}
+		
+	}
+
+	public function getProducts(){
+
+		$Sql = new Sql();
+
+		$rows = $Sql->select("
+		SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(B.vlprice) AS vltotal
+		FROM tb_cartsproducts a 
+		INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+		WHERE a.idcart = :idcart AND a.dtremoved is NULL 
+		GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
+		ORDER BY b.desproduct", [
+			":idcart"=>$this->getidcart()
+		]);
+
+		return Product::checkList($rows);
+
+	}
+
 }
 
 ?>
--- views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/category.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -27,7 +27,7 @@
                     </div>  
                     
                     <div class="product-option-shop">
-                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Comprar</a>
+                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add">Comprar</a>
                     </div>                       
                 </div>
             </div>
@@ -42,9 +42,7 @@
                         <ul class="pagination">
                         <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
 
-
                         <li><a href="<?php echo htmlspecialchars( $value1["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
-                        
                         <?php } ?>
 
                         </ul>
