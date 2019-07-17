b826c06de92b0d303f5450db3a05c0c4922c28d9
Cleiver Dutra <cleiverad@gmail.com>
2019-07-17 16:39:58 -0300

    Calculo do Frete

--- site.php
+++ site.php
@@ -72,7 +72,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		"cart"=>$cart->getValues(),
-		"products"=>$cart->getProducts()
+		"products"=>$cart->getProducts(),
+		"error"=>''
 	]); 
 
 });
@@ -124,6 +125,18 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+
+	exit;
+
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST["zipcode"]);
 
 	header("Location: /cart");
 
